e3eb1e3b26991d260ffd9cca3739e92fcad9dac7
Sandro Martins da Costa <profsandromartins@gmail.com>
2022-10-07 08:54:28 -0300

    header

--- includes/_header.php
+++ includes/_header.php
@@ -9,18 +9,27 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/style.css?v=<?php echo rand(0, 9999999); ?>">
     <title>Jornal da Escola</title>
 
 </head>
 
 <body>
     <header>
-        <nav>
-            <ul>
-                <li><a href="./index.php">Index</a></li>
-                <li><a href="./editoria.php">Editoria</a></li>
-                <li><a href="./noticia.php">Notícia</a></li>
-            </ul>
-        </nav>
+        <div class="pos-f-t">
+            <div class="collapse" id="navbarToggleExternalContent">
+                <div class="bg-dark p-4">
+                    <h4 class="text-white">Collapsed content</h4>
+                    <span class="text-muted">Toggleable via the navbar brand.</span>
+                </div>
+            </div>
+            <nav class="navbar navbar-dark ">
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <h1>Atualiza ai</h1>
+            </nav>
+        </div>
     </header>
